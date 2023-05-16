08206c91217eade8c9b32fb29d8b3554e7150d5c
adrian <adrianlm1985@gmail.com>
2023-05-16 13:50:17 +0200

    Diseño de barra lateral, colores definidos

---
+++ NBA-Database/NBA-Database/Shared/NavMenu.razor.cs
@@ -0,0 +1,12 @@
+namespace NBA_Database.Shared
+{
+    public partial class NavMenu
+    {
+        private bool collapseNavMenu = true;
+        private string? NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+        private void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+    }
+}
