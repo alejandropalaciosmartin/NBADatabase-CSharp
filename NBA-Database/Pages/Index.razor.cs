7caa0c10cd8ba9300d859150c1742e422cb1abb1
adrian <adrianlm1985@gmail.com>
2023-05-18 21:00:44 +0200

    Pequeños cambios

---
+++ NBA-Database/Pages/Index.razor.cs
@@ -0,0 +1,24 @@
+using System.Net.Http.Json;
+
+namespace NBA_Database.Pages
+{
+    public partial class Index
+    {
+        /*private WeatherForecast[]? forecasts;
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("assets/data/data.csv");
+        }
+
+        public class WeatherForecast
+        {
+            public DateOnly Date { get; set; }
+
+            public int TemperatureC { get; set; }
+
+            public string? Summary { get; set; }
+
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        }*/
+    }
+}
