c9ebaea8f90ba2f9ebb5e1bc1f22f25858568eec
adrian <adrianlm1985@gmail.com>
2023-05-24 16:01:39 +0200

    Reestructuracion

--- NBA-Database/Pages/Index.razor.cs
+++ NBA-Database/Pages/Index.razor.cs
@@ -2,9 +2,12 @@ using System.Net.Http.Json;
 
 namespace NBA_Database.Pages
 {
+    //Para leer el archivo json 
     public partial class Index
     {
+        //Metemos en una raíz porque el tamaño no cambiará de PlayersName
         private PlayersName[]? players;
+        //Leemos y pasamos a players el cotnenido de json
         protected override async Task OnInitializedAsync()
         {
             players = await Http.GetFromJsonAsync<PlayersName[]>("/assets/data/dataPrueba.json");
@@ -54,53 +57,35 @@ namespace NBA_Database.Pages
             }
 
             //Para que aparezca la imagen del equipo 
-            /*public string daImagen()
+            public string DaImagen()
             {
-                    string imagen = "";
-                    string directorio = @"/assets/image/equipos";
-                try
-                {
-                    string team = $"{Team_abbreviation}.png";
-
-                    if (Path.Exists(directorio))
-                    {
-                        string[] archivos = Directory.GetFiles(directorio, "*.png");
-
-                        foreach (var dir in archivos)
-                        {
-                            if (dir == team)
-                            {
-                                imagen = $"/assets/image/equipos/{Team_abbreviation}.png";
-                            }
-                            else
-                            {
-                                imagen = $"/assets/image/mundo.png";
-                            }
-
-                        }
-
-                    }
-                    else
-                    {
-                        Console.WriteLine("No existe");
-                    }
-                    //    string imagen = "";
-                    //if ($"/assets/image/equipos/{Team_abbreviation}.png" == $"/assets/image/equipos/*.png")
-                    //{
-                    //    imagen = $"/assets/image/equipos/{Team_abbreviation}.png";
-                    //}
-                    //else
-                    //{
-                    //    imagen = $"/assets/image/equipos/mundo.png";
-                    //}
-                }
-                catch(Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
+                //Creamos array con los equipos que tenemos la imagen
+                 string[] imagenExiste = { "CHI", "LAC", "TOR" };
+                //Imagen de salida que la obtenemos en el foreach del Index.razor al llamar al método
+                 string imagen = "";
+             try
+             {
+                    //Recorremos el array de las fotos existentes
+                 for (int i = 0; i < imagenExiste.Length; i++) 
+                 {
+                        //Miramos si el valor del json que leemos lo contiene el array, si lo contiene lo muestra y sino muestra una predeterminada
+                     if (imagenExiste.Contains(Team_abbreviation))
+                     {
+                         imagen = $"/assets/image/equipos/{Team_abbreviation}.png";
+                     }
+                     else
+                     {
+                         imagen = $"/assets/image/mundo.png";
+                     }
+                 }
+             }
+             catch(Exception ex)
+             {
+                 Console.WriteLine(ex.Message);
+             }
                 return imagen;
-            }*/
-            public string daImagenPais() => $"/assets/image/equipos/{Country}.png";
+            }
+            //public string DaImagenPais() => $"/assets/image/equipos/{Country}.png";
         }
     }
 }
